f1037d6a3a57cf8d1c48ce5d6264011bd6f50082
ac-peschel <mail@acpeschel.net>
2026-05-23 21:20:57 +0200

    added title

--- main.v
+++ main.v
@@ -4,7 +4,7 @@ import rand
 
 fn is_behind_flower(x f32, y f32, cx int, cy int) bool {
    p_width := get_line_length()
-   p_height := get_peony().len / get_line_length()
+   p_height := (get_peony().len / get_line_length() + 16)
    interfers_h := (x > cx + (p_width / 2))
       || (x < cx - (p_width / 2))
    interfers_v := (y > cy + (p_height / 2))
@@ -29,7 +29,7 @@ fn main() {
       s.update_size(width, height)
       center_x := width / 2
       center_y := height / 2
-      if width < get_line_length() || height < (get_peony().len /get_line_length()) {
+      if width < get_line_length() || height < (get_peony().len /get_line_length() + 8) {
          s.print_text_centered("terminal too small", 0)
       } else {
          s.set_fg_color(TermColor.white)
@@ -65,6 +65,12 @@ fn main() {
 
          s.set_fg_color(TermColor.magenta)
          s.print_centered_object(get_peony(), get_line_length())
+
+         s.set_fg_color(TermColor.red)
+         s.draw_heart(width, height, 40)
+         s.draw_heart(width, height, -40)
+         s.set_fg_color(TermColor.white)
+         s.draw_title(width, height)
       }
 
       time.sleep(16 * time.millisecond)
--- screen.v
+++ screen.v
@@ -51,8 +51,50 @@ fn (s Screen) print_centered_object(obj string, cols int) {
 	center := lines.len / 2
 	for i, line in lines {
 		y_offset := i - center
-		s.print_text_centered(line, y_offset)
+		s.print_text_centered(line, y_offset + 8)
 	}
+}
+
+fn (s Screen) draw_title(width int, height int) {
+	center_x := width / 2
+	center_y := height / 2
+	flower_height := get_peony().len / get_line_length()
+	mut vert_offset := center_y - (flower_height / 2) - 6
+	if vert_offset < 3 {
+		vert_offset = 3
+	}
+	fx := center_x - 22
+	s.move_cursor(fx, vert_offset)
+	print(" _____ _   _ ____     ____ _____ _     ___ _   _    _   ")
+	s.move_cursor(fx, vert_offset + 1)
+	print("|  ___(_) (_)  _ \\   / ___| ____| |   |_ _| \\ | |  / \\  ")
+	s.move_cursor(fx, vert_offset + 2)
+	print("| |_  | | | | |_) | | |   |  _| | |    | ||  \\| | / _ \\   ")
+	s.move_cursor(fx, vert_offset + 3)
+	print("|  _| | |_| |  _ <  | |___| |___| |___ | || |\\  |/ ___ \\  ")
+	s.move_cursor(fx, vert_offset + 4)
+	print("|_|    \\___/|_| \\_\\  \\____|_____|_____|___|_| \\_/_/   \\_\\")
+}
+
+fn (s Screen) draw_heart(width int, height int, h_offset int) {
+	center_x := width / 2
+	center_y := height / 2
+	flower_height := get_peony().len / get_line_length()
+	mut vert_offset := center_y - (flower_height / 2) - 6
+	if vert_offset < 3 {
+		vert_offset = 3
+	}
+	fx := center_x + h_offset
+	s.move_cursor(fx, vert_offset)
+	print(",d88b.d88b,")
+	s.move_cursor(fx, vert_offset + 1)
+	print("88888888888")
+	s.move_cursor(fx, vert_offset + 2)
+	print("`Y8888888Y'")
+	s.move_cursor(fx, vert_offset + 3)
+	print("  `Y888Y'")
+	s.move_cursor(fx, vert_offset + 4)
+	print("    `Y'")
 }
 
 fn (s Screen) erase_particle(p Particle) {
